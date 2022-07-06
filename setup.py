73b2a6657fb05d940a992cf3b61a4d5971e50533
Soloman J <soloman.jarman@gmail.com>
2022-07-06 16:23:44 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Embedded Systems',
+    'Topic :: System :: Hardware'
+    ]
+
+setup(name="labjack",
+      version='1.0.0',
+      description='LabJack Python wrappers',
+      url='https://labjack.com/support/software/',
+      author='LabJack Corporation',
+      author_email="support@labjack.com",
+      maintainer='LabJack Corporation',
+      maintainer_email='support@labjack.com',
+      classifiers=CLASSIFIERS,
+      license='MIT X11',
+      packages=[],
+      install_requires=[
+        'labjack-ljm',
+        'LabJackPython',
+      ],
+      )
